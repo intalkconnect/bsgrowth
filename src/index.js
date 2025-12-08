7732d01ecab3535de21e3aab591caf92e34c0a4b
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-12-08 19:09:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
-
-const port = process.env.PORT || 3000;
+const { PORT } = require('./config/env');
 
 app.listen(PORT, () => {
   console.log(`API rodando na porta ${PORT}`);
